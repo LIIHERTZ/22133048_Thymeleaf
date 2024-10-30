dd973e9baa2babac67d32aab5838c76e1102ac2d
Dương Thành Sơn <duongthanhson2004@gmail.com>
2024-10-30 09:16:50 +0700

    finish thymeleaf

---
+++ 22133048_DuongThanhSon_Thymeleaf/22133048_DuongThanhSon_Thymeleaf/src/main/java/DTS/DemoThymeleafApplication.java
@@ -0,0 +1,12 @@
+package DTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoThymeleafApplication.class, args);
+	}
+}
